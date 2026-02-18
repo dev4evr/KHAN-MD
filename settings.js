5df6194170ac3cbb7c43d3bc2be7ed4cf4890c0b
IsHaQ VII <90872109+dev4evr@users.noreply.github.com>
2026-02-18 12:07:14 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@
 module.exports = {
   SESSION_ID: "",  // add session Id 
   
-  OWNER_NUMBER: "923427582273", // owner number 
+  OWNER_NUMBER: "212637915359", // owner number 
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
